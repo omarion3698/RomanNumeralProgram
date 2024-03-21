612b2c304fa5f65d0a00d515b8c5a39b6b82e569
omarion3698 <omaribinbakarivic@gmail.com>
2024-03-21 16:13:43 +0300

    comments added

--- RomanNumeral.java
+++ RomanNumeral.java
@@ -2,37 +2,58 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class RomanNumeral {
-    private static final Map<Character, Integer> map;
+    private static final Map<Character, Integer> map; 
 
     static {
+        // Creating a temporary HashMap.
         Map<Character, Integer> tempMap = new HashMap<>();
+        // Putting a mapping of character 'I' to integer 1.
         tempMap.put('I', 1);
+        // Putting a mapping of character 'V' to integer 5.
         tempMap.put('V', 5);
+        // Putting a mapping of character 'X' to integer 10.
         tempMap.put('X', 10);
+        // Putting a mapping of character 'L' to integer 50.
         tempMap.put('L', 50);
+        // Putting a mapping of character 'C' to integer 100.
         tempMap.put('C', 100);
+        // Putting a mapping of character 'D' to integer 500.
         tempMap.put('D', 500);
+        // Putting a mapping of character 'M' to integer 1000.
         tempMap.put('M', 1000);
+        // Making the map immutable using Collections.unmodifiableMap and assigning it to 'map'.
         map = Collections.unmodifiableMap(tempMap);
     }
-
+    // Method to convert a Roman numeral to an integer.
     public int romanToInt(String s) {
+        // Initializing the converted number as 0.
         int convertedNumber = 0;
+        // Looping through each character of the input string.
         for (int i = 0; i < s.length(); i++) {
+            // Getting the integer value corresponding to the current character.
             int currentNumber = map.get(s.charAt(i));
+            // Getting the integer value of the next character, or 0 if there's no next character.
             int next = i + 1 < s.length() ? map.get(s.charAt(i + 1)) : 0;
+            // Checking if the current number is greater than or equal to the next number.
             if (currentNumber >= next) {
+                // Adding the current number to the converted number.
                 convertedNumber += currentNumber;
             } else {
+                // Subtracting the current number from the converted number.
                 convertedNumber -= currentNumber;
             }
         }
         return convertedNumber;
     }
+
     public static void main(String[] args) {
+        // Creating an instance of the RomanNumeral class.
         RomanNumeral romanConverter = new RomanNumeral();
+        // Converting the Roman numeral "X" to an integer.
         int result = romanConverter.romanToInt("X");
+        // Printing the result.
         System.out.println(result);
     }
 }
--- RomanNumeralTest.java
+++ RomanNumeralTest.java
@@ -1,17 +1,29 @@
-public class RomanNumeralTest {
+// Declaring a class named RomanNumeralTest for testing RomanNumeral class.
+public class RomanNumeralTest { 
     @Test
+    // Method to test single-digit Roman numerals.
     void singleDigit() {
+        // Asserting that the Roman numeral "I" converts to integer 1.
         Assertions.assertEquals(1, new RomanNumeral().romanToInt("I"));
+        // Asserting that the Roman numeral "V" converts to integer 5. 
         Assertions.assertEquals(5, new RomanNumeral().romanToInt("V"));
+        // Asserting that the Roman numeral "X" converts to integer 10.
         Assertions.assertEquals(10, new RomanNumeral().romanToInt("X"));
+        // Asserting that the Roman numeral "L" converts to integer 50.
         Assertions.assertEquals(50, new RomanNumeral().romanToInt("L"));
+        // Asserting that the Roman numeral "C" converts to integer 100.
         Assertions.assertEquals(100, new RomanNumeral().romanToInt("C"));
+        // Asserting that the Roman numeral "D" converts to integer 500.
         Assertions.assertEquals(500, new RomanNumeral().romanToInt("D"));
+        // Asserting that the Roman numeral "M" converts to integer 1000.
         Assertions.assertEquals(1000, new RomanNumeral().romanToInt("M"));
     }
-    @Test
-    void repetition() {
+
+    @Test // Denoting that the following method is a test method.
+    void repetition() { // Method to test repetition of Roman numerals.
+        // Asserting that the Roman numeral "II" converts to integer 2.
         Assertions.assertEquals(2, new RomanNumeral().romanToInt("II"));
+        // Asserting that the Roman numeral "XX" converts to integer 20.
         Assertions.assertEquals(20, new RomanNumeral().romanToInt("XX"));
     }    
 }
